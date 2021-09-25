bc54c5c98dc7024f84f503bbecfbde7a7c08c9d4
Klapeks <41518563+Klapeks@users.noreply.github.com>
2021-09-26 02:51:25 +0300

    Some removes :(

--- start.bat
+++ start.bat
@@ -1,13 +1,4 @@
 :start 
 node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
-node --trace-warnings --require ts-node/register index.ts
 pause
 goto start
